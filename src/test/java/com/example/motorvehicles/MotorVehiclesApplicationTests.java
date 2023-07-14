b1f3bb9f529b77b50eba3f5bf4aa3c03eb52ed72
Елизавета <katkova_lizochka@bk.ru>
2023-07-14 15:09:22 +0300

    Initial commit

---
+++ src/test/java/com/example/motorvehicles/MotorVehiclesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.motorvehicles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotorVehiclesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
