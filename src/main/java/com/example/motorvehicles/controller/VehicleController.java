7a4359c30ae45c28ae0b9a92a873a9bd753b1138
Елизавета <katkova_lizochka@bk.ru>
2023-07-15 13:04:41 +0300

    Можно добавить новое транспортное средство

--- src/main/java/com/example/motorvehicles/controller/VehicleController.java
+++ src/main/java/com/example/motorvehicles/controller/VehicleController.java
@@ -1,16 +1,15 @@
 package com.example.motorvehicles.controller;
 
+import com.example.motorvehicles.model.Vehicle;
 import com.example.motorvehicles.repository.VehicleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/vehicles")
 public class VehicleController {
-
     @Autowired
     private VehicleRepository repository;
 
@@ -22,7 +21,13 @@ public class VehicleController {
 
     @GetMapping("/add")
     public String addVehicle(Model model) {
-        model.addAttribute("vehicleList", repository.findAll());
-        return "vehicles-list";
+        model.addAttribute("vehicle", new Vehicle());
+        return "vehicle-add";
+    }
+
+    @PostMapping("/add")
+    public String addVehiclePost(@ModelAttribute("vehicle") Vehicle vehicle) {
+        repository.save(vehicle);
+        return "redirect:/vehicles";
     }
 }
