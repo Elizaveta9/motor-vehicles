7b293ad703eb9974f2fac1bcbf682d03c29087df
Елизавета <katkova_lizochka@bk.ru>
2023-07-15 14:22:11 +0300

    Добавлена валидация, на главной странице в столбце "Наличие прицепа" теперь вместо true и false отображается "имеется", "не имеется"

--- src/main/java/com/example/motorvehicles/controller/VehicleController.java
+++ src/main/java/com/example/motorvehicles/controller/VehicleController.java
@@ -2,10 +2,15 @@ package com.example.motorvehicles.controller;
 
 import com.example.motorvehicles.model.Vehicle;
 import com.example.motorvehicles.repository.VehicleRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/vehicles")
@@ -26,7 +31,10 @@ public class VehicleController {
     }
 
     @PostMapping("/add")
-    public String addVehiclePost(@ModelAttribute("vehicle") Vehicle vehicle) {
+    public String addVehiclePost(@ModelAttribute("vehicle") @Valid Vehicle vehicle, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "vehicle-add";
+        }
         repository.save(vehicle);
         return "redirect:/vehicles";
     }
--- src/main/java/com/example/motorvehicles/model/Vehicle.java
+++ src/main/java/com/example/motorvehicles/model/Vehicle.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.*;
 import lombok.*;
 
 @Entity
@@ -15,11 +16,18 @@ public class Vehicle {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @NotBlank(message = "Впишите марку")
     private String brand;
+    @NotBlank(message = "Впишите модель")
     private String model;
+    @NotBlank(message = "Впишите категорию")
     private String category;
+    @NotBlank(message = "Впишите государсвенный номер")
+    @Pattern(regexp = "[а-яА-Я]{2}\\d\\d\\d[а-яА-Я]", message = "Государственный номер записан некорректно")
     private String licenseNumber;
+    @NotBlank(message = "Впишите тип транспортного средства")
     private String type;
+    @NotNull(message = "Впишите год выпуска")
     private Integer releaseYear;
     private Boolean hasTrailer;
 }
