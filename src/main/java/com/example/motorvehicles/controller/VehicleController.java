a88de47d9d74bce986058e12bca5d2db3ea5a73b
Елизавета <katkova_lizochka@bk.ru>
2023-07-16 15:18:33 +0300

    Добавлен поиск

--- src/main/java/com/example/motorvehicles/controller/VehicleController.java
+++ src/main/java/com/example/motorvehicles/controller/VehicleController.java
@@ -2,12 +2,19 @@ package com.example.motorvehicles.controller;
 
 import com.example.motorvehicles.model.Vehicle;
 import com.example.motorvehicles.repository.VehicleRepository;
+import com.example.motorvehicles.repository.VehicleSpecification;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Predicate;
 
 @Controller
 @RequestMapping("/vehicles")
@@ -16,8 +23,26 @@ public class VehicleController {
     private VehicleRepository repository;
 
     @GetMapping
-    public String vehiclesList(Model model) {
-        model.addAttribute("vehicleList", repository.findAll());
+    public String vehiclesList(Model modelTH,
+                               @RequestParam(defaultValue = "") String brand,
+                               @RequestParam(defaultValue = "") String model,
+                               @RequestParam(defaultValue = "") String category,
+                               @RequestParam(defaultValue = "") String licenseNumber,
+                               @RequestParam(defaultValue = "") String releaseYear) {
+        Map<String, String> searchParameters = new HashMap<>();
+        if (!brand.isEmpty()) searchParameters.put("brand", brand);
+        if (!model.isEmpty()) searchParameters.put("model", model);
+        if (!category.isEmpty()) searchParameters.put("category", category);
+        if (!licenseNumber.isEmpty()) searchParameters.put("licenseNumber", licenseNumber);
+        if (!releaseYear.isEmpty()) searchParameters.put("releaseYear", releaseYear);
+
+        Iterable<Vehicle> vehicles;
+        if (!searchParameters.isEmpty()) {
+            vehicles = repository.findAll(new VehicleSpecification().getSpecification(searchParameters));
+        } else {
+            vehicles = repository.findAll();
+        }
+        modelTH.addAttribute("vehicleList", vehicles);
         return "vehicles-list";
     }
 
--- src/main/java/com/example/motorvehicles/repository/VehicleRepository.java
+++ src/main/java/com/example/motorvehicles/repository/VehicleRepository.java
@@ -1,10 +1,15 @@
 package com.example.motorvehicles.repository;
 
 import com.example.motorvehicles.model.Vehicle;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface VehicleRepository extends CrudRepository<Vehicle, Long> {
+    List<Vehicle> findByBrand(String brand);
 
+    List<Vehicle> findAll(Specification<Vehicle> specification);
 }
